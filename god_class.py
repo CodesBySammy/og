86ec0334eee9a18127e027fe4f16468b1353b5a0
Sameer Mahindru <113050438+CodesBySammy@users.noreply.github.com>
2026-04-04 21:18:32 +0530

    Create GodClass with basic data handling methods

---
+++ god_class.py
@@ -0,0 +1,18 @@
+class GodClass:
+    def __init__(self):
+        self.data = []
+
+    def read(self):
+        print("Reading data")
+
+    def process(self):
+        print("Processing data")
+
+    def save(self):
+        print("Saving data")
+
+    def send_email(self):
+        print("Sending email")
+
+    def generate_report(self):
+        print("Generating report")
